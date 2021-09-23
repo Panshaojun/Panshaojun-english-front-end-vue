cfca76b7607e2404c7704287c14274f6ba710578
潘少军 <1760406005@qq.com>
2021-09-23 16:36:25 +0800

    fix 解决不会根据日期立即刷新的问题

--- src/views/review-today/index.vue
+++ src/views/review-today/index.vue
@@ -1,40 +1,45 @@
 <template>
-  <a-row>
-    <a-col :span="3"></a-col>
-    <a-col :span="18">
-      <a-divider orientation="left">
-        <h1>今日复习</h1>
-      </a-divider>
-      <a-col :span="24">
-        <a-date-picker v-model:value="date" @change="changeDate()" />
+  <div id="review-tody">
+    <a-row>
+      <a-col :span="3"></a-col>
+      <a-col :span="18">
+        <a-divider orientation="left">
+          <h1>今日复习</h1>
+        </a-divider>
+        <a-col :span="24">
+          <a-date-picker v-model:value="date" @change="changeDate()" />
+        </a-col>
+        <a-col :span="24">
+          <a-list item-layout="horizontal" :data-source="data">
+            <template #renderItem="{ item }">
+              <a-list-item>
+                <div class="review-list">
+                  <div class="date">{{ item.date }}</div>
+                  <div class="nums">{{ item.words }}</div>
+                  <div class="go-review">
+                    <a-button @click="toReview(item.id)">复习</a-button>
+                  </div>
+                </div>
+              </a-list-item>
+            </template>
+          </a-list>
+        </a-col>
       </a-col>
-      <a-list item-layout="horizontal" :data-source="data">
-        <template #renderItem="{ item }">
-          <a-list-item>
-            {{ item.date }}:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{
-              item.words
-            }}
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a-button
-              @click="toReview(item.id)"
-              >开始学习</a-button
-            >
-          </a-list-item>
-        </template>
-      </a-list>
-    </a-col>
-  </a-row>
+    </a-row>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useRouter } from "vue-router";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { useStore } from "vuex";
 import { key } from "@/store";
 import moment from "moment";
 const store = useStore(key);
 const router = useRouter();
-const data = ref(store.getters["review/day"]());
+const data = ref<any>();
 store.dispatch("review/freshData");
+
 const date = ref(moment());
 const changeDate = () => {
   const chooseDate = date.value
@@ -42,8 +47,26 @@ const changeDate = () => {
     : moment().format("Y-MM-DD");
   data.value = store.getters["review/day"](chooseDate);
 };
+watch(() => store.state.review.data, changeDate);
 const toReview = (id: number) => {
   store.dispatch("review/freshReviewData", id);
   router.push("/review");
 };
 </script>
+
+<style lang="scss">
+#review-tody {
+  .review-list {
+    display: flex;
+    .date {
+      line-height: 40px;
+      flex: 0 0 100px;
+    }
+    .nums {
+      line-height: 40px;
+      flex: 0 0 50px;
+      font-weight: bold;
+    }
+  }
+}
+</style>
